671a2e7b3f63bf56279812b871e153433b6c1861
Jozek <jnowakpiano@gmail.com>
2025-09-26 22:48:50 -0500

    Assignment done

--- a2.py
+++ a2.py
@@ -21,30 +21,63 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
     # keep checking as long as we haven't hit the end of either pattern or source while
     # pind is still a valid index OR sind is still a valid index (valid index means that
     # the index is != to the length of the list)
-    while "FILL IN CONDITION HERE":
+    while pind < len(pattern) or sind < len(source):
         # your job is to fill out the body of this loop
 
-        # you should delete the following line
-        return ["Not done yet :)"]
-
         # 1) if we reached the end of the pattern but not source
-
+        if pind == len(pattern):
+            # print("reached end of pattern, but not source - return none")
+            return None
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
+        elif pattern[pind] == "%":
+            # Two situation, % is last, and % is not last
+            if pind == len(pattern) - 1:
+                combined = " ".join(source[sind:])
+                result.append(combined)
+                # print(result)
+                return result
+            else: # not the last element of pattern
+                pind += 1
+                slocation = sind
+                while pattern[pind] != source[sind]:
+                    sind += 1
+
+                    if sind == len(source):
+                        return None
+
+                # print(sind, slocation)
+                result.append(" ".join(source[slocation:sind]))
+                # print(result)
 
         # 3) if we reached the end of the source but not the pattern
-
+        elif sind == len(source):
+            # print("reached end of source, but not pattern - return none")
+            return None
         # 4) if the current thing in the pattern is an _
-
+        elif pattern[pind] == "_":
+            # print(pattern[pind], source[sind])
+            result.append(source[sind])
+            pind += 1
+            sind += 1
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
+        elif pattern[pind] == source[sind]:
+            # print(pattern[pind], source[sind])
+            pind += 1
+            sind += 1
 
         # 6) else : this will happen if none of the other conditions are met it
         # indicates the current thing it pattern doesn't match the current thing in
         # source
+        else:
+            # print(pattern[pind] + " " + source[sind])
+            # print("no match")
+            return None
 
+    # print("successful match")
     return result
 
 
@@ -56,6 +89,8 @@ if __name__ == "__main__":
     assert match(["x", "_", "z"], ["x", "y", "z"]) == ["y"], "test 5 failed"
     assert match(["x", "_", "_"], ["x", "y", "z"]) == ["y", "z"], "test 6 failed"
     assert match(["%"], ["x", "y", "z"]) == ["x y z"], "test 7 failed"
+    assert match(["x", "%"], ["x", "y", "z", "a", "b"]) == ["y z a b"], "extra test 7 failed"
+
     assert match(["x", "%", "z"], ["x", "y", "z"]) == ["y"], "test 8 failed"
     assert match(["%", "z"], ["x", "y", "z"]) == ["x y"], "test 9 failed"
     assert match(["x", "%", "y"], ["x", "y", "z"]) == None, "test 10 failed"
--- a2_writeup.md
+++ a2_writeup.md
@@ -13,8 +13,11 @@ This assignment is about learning and applying the while loop and iterating thro
 ## Reflection Questions
 1. What was difficult for you while completing the match function?
 
-
+The hardest part was dealing with the %, it can mean any num. Meaning i had to be careful when parsing through the text in order to not skip too much or run out of things to check. It got a little harder when mixing % and _ since i had to make sure that the matches were right.
 
 2. Explain how you could use the match function for extracting information from a movie database.
+
+The match function works like a pattern matcher. You can use _ to grab one word, or % for several. These different pieces can be then used to search through the database or build a structred query.
+
 
 
